9dbb30b18914bc6d65c43359e915a4be2622a0fb
Loporrit <141286461+loporrit@users.noreply.github.com>
2025-03-02 03:59:39 +0000

    Add BunnyWhispers.Tests

---
+++ BunnyWhispers.Tests/KeyTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BunnyWhispers.Tests;
+
+[TestClass]
+public class KeyTests
+{
+    [TestMethod]
+	public void Dummy()
+	{
+		Assert.IsTrue(true);
+	}
+}
